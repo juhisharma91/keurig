3a3438462d6b5d5091e1563002866b252fa4d277
juhi.sharma <juhi.sharma@TVMATP403905D.ad.infosys.com>
2016-12-12 17:14:45 +0530

    changed

--- KEURIG/src/loginPage/loginCheck.java
+++ KEURIG/src/loginPage/loginCheck.java
@@ -5,7 +5,8 @@ public class loginCheck {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
-		System.out.println("changed code");
+		System.out.println(" code");
+		System.out.println(" added one more line of code");
 
 	}
 
