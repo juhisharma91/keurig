40469a692634a099d1677de119de070374dd2346
syedismailfaiyaz.s <syedismailfaiyaz.s@infosys.com>
2016-12-12 11:09:31 +0530

    Done

---
+++ KEURIG/src/loginPage/loginCheck.java
@@ -0,0 +1,11 @@
+package loginPage;
+
+public class loginCheck {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Login page");
+
+	}
+
+}
